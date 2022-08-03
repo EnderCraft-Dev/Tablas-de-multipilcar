76f7d84e314707e67d494ceb9e7629c7caa58888
EnderCraft-Dev <4soles4@gmail.com>
2022-08-03 13:07:18 -0500

    Game v2.0

--- src/game.cpp
+++ src/game.cpp
@@ -14,44 +14,20 @@ int randomMultiplyNumbers[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 void title();
 int inputNumber();
 int randomIndex(int n);
+void game();
+float calculateScore(int score, int possibleAnswers);
 
 int main()
 {
-	// Random seed
-	srand(time(NULL));
-
 	// Title
 	title();
 
-	// Game logic
-	int attempts;
-	int counter = 0;
-	cout << "Ingresa cuantas preguntas quieres responder: "; cin >> attempts;
-
-	int startNumber = inputNumber();
-
-	while (counter < attempts)
-	{
-		int index = randomIndex(12);
-		int userAnswer;
-		int numberToMultiply = randomMultiplyNumbers[index];
-
-		int correctAnswer = startNumber * numberToMultiply;
-		cout << startNumber << "x" << numberToMultiply << "="; cin >> userAnswer;
-		if (userAnswer == correctAnswer)
-		{
-			cout << "Correcto!" << endl;
-		}
-		else {
-			cout << "Incorrecto! La respuesta correcta es " << correctAnswer << endl;
-		}
-		counter++;
-		cout << endl;
-	}
+	// Starts the game
+	game();
+	
 	
 
 	// Avoids the program to close when finished
-	cout << "Juego terminado!" << endl;
 	system("pause");
 
 	return 0;
@@ -94,4 +70,46 @@ int inputNumber()
 int randomIndex(int n)
 {
 	return rand() % n;
+}
+
+float calculateScore(int score, int possibleAnswers)
+{
+	float percentage = (score*100)/possibleAnswers;
+	return percentage;
+}
+
+// Game logic
+void game()
+{
+	srand(time(NULL));
+
+	int score = 0;
+
+	int attempts;
+	int counter = 0;
+	cout << "Ingresa cuantas preguntas quieres responder: "; cin >> attempts;
+
+	int startNumber = inputNumber();
+
+	while (counter < attempts)
+	{
+		int index = randomIndex(12);
+		int userAnswer;
+		int numberToMultiply = randomMultiplyNumbers[index];
+
+		int correctAnswer = startNumber * numberToMultiply;
+		cout << startNumber << "x" << numberToMultiply << "="; cin >> userAnswer;
+		if (userAnswer == correctAnswer)
+		{
+			cout << "Correcto!" << endl;
+			score++;
+		}
+		else {
+			cout << "Incorrecto! La respuesta correcta es " << correctAnswer << endl;
+		}
+		counter++;
+		cout << endl;
+	}
+	float percentage = calculateScore(score, attempts);
+	cout << "Juego terminado! Contestaste correctamente el " << percentage << "% de las preguntas." << endl;
 }
